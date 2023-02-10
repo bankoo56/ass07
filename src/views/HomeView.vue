49d457109c07e98377ca25412158f55961f1594c
bankoo56 <bankoo525@gmail.com>
2023-02-11 03:56:43 +0700

    hi

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,47 +4,88 @@ import TheWelcome from '../components/TheWelcome.vue'
 
 <template>
 
-<div style="display: flex; justify-content: center; margin-bottom: 5vh;">
-  <div class="card" style="width: 50rem; box-shadow: 5px 5px 30px; background-color: whitesmoke;">
-    <div class="text-center font-weight-bold text-uppercase" style="font-size: 25px; margin-top: 1rem;">
-      <i class="bi bi-megaphone"></i>
-      announcement
-      <i class="bi bi-megaphone"></i></div>
-    <div class="card-body">
-    <div id="carouselExample" class="carousel slide">
-      <div class="carousel-inner" style="border-radius: 5px;">
-        <div class="carousel-item active">
-          <img src="https://scontent.fbkk7-3.fna.fbcdn.net/v/t39.30808-6/328625852_461688432698657_4447092139426367137_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=730e14&_nc_ohc=uie2oegAdzMAX9zdHeJ&_nc_ht=scontent.fbkk7-3.fna&oh=00_AfDgyk-RDlT41H-JRRl4-hi6Nb9LzDVaZk5S83qxcwdtQQ&oe=63E33F77" class="d-block w-100" alt="..." style="height: 50rem;">
-        </div>
-        <div class="carousel-item">
-          <img src="https://scontent.fbkk7-3.fna.fbcdn.net/v/t39.30808-6/313030598_416455580690771_365749320803407479_n.jpg?stp=dst-jpg_s640x640&_nc_cat=105&ccb=1-7&_nc_sid=730e14&_nc_ohc=cCWMaG31w2cAX9A6Cn6&_nc_oc=AQnFsoLce_DpY_PTs4MH36VRnljK-UW5Ch6ek1hR3dYq_5VcpoQyYzlZOYW_z_xRymc&_nc_ht=scontent.fbkk7-3.fna&oh=00_AfAQ2X4VWYtYSeunJ12JFRyTt7MAeiBSq-fDov4EcWxxbg&oe=63E37717" class="d-block w-100" alt="..." style="height: 50rem;">
-        </div>
-        <div class="carousel-item">
-          <img src="https://scontent.fbkk7-2.fna.fbcdn.net/v/t39.30808-6/328674537_877668393381157_6924670978012438076_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=730e14&_nc_ohc=DXuTECh8Xv4AX_TC1u1&_nc_ht=scontent.fbkk7-2.fna&oh=00_AfD1HoZoIAVBhJAG5ehKIdkyUMWX4390207qdDd34yFGTw&oe=63E3B764" class="d-block w-100" alt="..." style="height: 50rem;">
-        </div>
-        <div class="carousel-item">
-          <img src="https://www.mju.ac.th/th/images/BN_News1.jpg" class="d-block w-100" alt="..." style="height: 50rem;">
+  <div style="margin-top: 8vh;">
+    <div class="card"
+      style="width: 100%; box-shadow: 5px 5px 30px; background-color: whitesmoke; padding: 0vh 2vh 5vh 2vh;">
+      <p class="text-center font-weight-bold text-uppercase" style="font-size: 25px; margin-top: 2rem;">
+        <i class="bi bi-megaphone"></i>
+        NEWS
+        <i class="bi bi-megaphone"></i>
+      </p>
+      <div class="card-body">
+        <div id="carouselExample" class="carousel slide">
+          <div class="carousel-inner" style="border-radius: 30px; box-shadow: 5px 5px 10px;">
+            <div class="carousel-item active">
+              <img src="https://erp.mju.ac.th/picture/banner/26f811d39dd044cf96a860fcdab87a3d.jpg" class="d-block w-100"
+                alt="..." style="height: 25rem;">
+            </div>
+            <div class="carousel-item ">
+              <img src="https://erp.mju.ac.th/picture/banner/4647e6fc8c2444f1a9ab26e4ab6de722.jpeg"
+                class="d-block w-100" alt="..." style="height: 25rem;">
+            </div>
+            <div class="carousel-item ">
+              <img src="https://erp.mju.ac.th/picture/banner/b910c6184cf846188c66a0de81f869f2.jpg" class="d-block w-100"
+                alt="..." style="height: 25rem;">
+            </div>
+            <div class="carousel-item ">
+              <img src="https://erp.mju.ac.th/picture/banner/f49ae4dedfb641c083d6fefe1bac01c4.jpg" class="d-block w-100"
+                alt="..." style="height: 25rem;">
+            </div>
+            <div class="carousel-item ">
+              <img src="https://erp.mju.ac.th/picture/banner/b5aaea3378d046f4beb7df2cf7962f46.jpg" class="d-block w-100"
+                alt="..." style="height: 25rem;">
+            </div>
+
+          </div>
+          <div style="padding-top: 3vh; ">
+            <p class="text-center">⬇ ติดตามข่าวสารเพิ่มเติมได้ที่นี่ ⬇</p>
+            <div class="text-center"
+              style="display: flex; margin: 2vh 10vh 0vh 10vh; text-shadow: 1px 0px 4px darkslategray; justify-content: space-between;">
+              <a href="https://www.facebook.com/MJUNow" target="_blank">
+                <h3 style="display: block;">
+                  <i class="bi bi-facebook" style="font-size:30px; color: blue;"></i>
+                  MJU Now <i class="bi bi-box-arrow-up-right"></i>
+                </h3>
+              </a>
+              <a href="https://www.instagram.com/maejouniversity/" target="_blank">
+                <h3 style="display: block;">
+                  <i class="bi bi-instagram" style="font-size:30px; color: purple;"></i>
+                  maejouniversity <i class="bi bi-box-arrow-up-right"></i>
+                </h3>
+              </a>
+              <a href="https://www.youtube.com/user/yuuppuuy" target="_blank">
+                <h3 style="display: block;">
+                  <i class="bi bi-youtube" style="font-size:30px; color: red;"></i>
+                  MAEJO UNIVERSITY <i class="bi bi-box-arrow-up-right"></i>
+                </h3>
+              </a>
+            </div>
+          </div>
+          <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+          </button>
+          <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+          </button>
         </div>
       </div>
-      <div class="text-center" style="margin: 1vh 0vh -1vh 3vh ; text-shadow: 1px 0px 4px darkslategray;">
-      ติดตามข่าวสารเพิ่มเติมได้ที่ ⬇
-      <a href="https://www.facebook.com/MJUNow" target="_blank">
-      <h3 style="display: block;">
-        <i class="bi bi-facebook" style="font-size:30px; color: blue;"></i>
-        MJU Now <i class="bi bi-box-arrow-up-right"></i>
-      </h3>
-      </a>
     </div>
-  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Previous</span>
-  </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Next</span>
-  </button>
   </div>
-  </div>
-  </div>
-</div>
 </template>
+
+<style scoped>
+* {
+  margin-left: auto;
+  margin-right: auto;
+  justify-content: center;
+  align-items: center;
+}
+
+p {
+  text-shadow: 1px 1px 5px;
+  font-style: italic;
+  font-size: 18px;
+}
+</style>
